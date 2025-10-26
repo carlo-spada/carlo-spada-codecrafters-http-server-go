9abf82cc4f35b32221eda9e0d205f85afc1a0091
Carlo Spada <carlo.spada22@gmail.com>
2025-10-26 14:53:54 -0600

    feat(server): respond with 200 OK
    Implements minimal HTTP/1.1 response using conn.Write.
    Request parsing not yet implemented.

--- app/main.go
+++ app/main.go
@@ -6,12 +6,7 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -19,11 +14,16 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
 	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println("Error accepting connection:", err)
 		os.Exit(1)
 	}
 	defer conn.Close()
+
+	if _, err := fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n\r\n"); err != nil {
+		fmt.Println("write failed:", err)
+	}
 }
